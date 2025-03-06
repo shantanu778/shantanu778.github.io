d1c2c2f3838f9008eb99a216e419ede148fde1c2
shantanu778 <shantunath778@gmail.com>
2025-03-06 10:52:11 +0000

    Deploying to gh-pages from @ shantanu778/shantanu778.github.io@0eb997b95216c9eb4542a81ada2df343e8538529 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -401,9 +401,9 @@ ninja.data = [{
           description: "Investigating Correlation between Brain Activation and Lanagugae Models Representation",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-explaining-machine-learning-models-for-sentiment-analysis-using-lime",
+          title: 'Explaining Machine Learning Models for Sentiment Analysis Using LIME',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
