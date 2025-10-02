2c95004a44e771bb91de885514ea0fe8996eec8f
shantanu778 <shantunath778@gmail.com>
2025-10-02 10:42:16 +0000

    Deploying to gh-pages from @ shantanu778/shantanu778.github.io@9f915d65eaa4f321f2eec8229a0f75116da5dbcd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -419,9 +419,9 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-",
+          title: '',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
             },},{id: "projects-project-5",
