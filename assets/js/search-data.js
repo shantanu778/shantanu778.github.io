48c49ac3b72a260e636f07043fb0c5532b57c872
shantanu778 <shantunath778@gmail.com>
2025-10-02 09:43:02 +0000

    Deploying to gh-pages from @ shantanu778/shantanu778.github.io@e6dfa4fa485d9031682d5c457a4a24a9ccdf8dd1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -414,9 +414,9 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-a-comparison-study-of-pre-trained-and-randomly-initialized-models-on-image-and-sequence-data",
+          title: 'A comparison study of pre-trained and randomly initialized models on image and sequence...',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
